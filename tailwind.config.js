62eb4e8d283af74e487495bbd3b291af3f746ded
Able <erugalev@objectstyle.com>
2020-01-19 18:44:11 +0300

    Ampjar Dashboard base markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,48 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {},
+    borderColor: theme => ({
+      ...theme('colors'),
+      default: theme('colors.gray.200', 'currentColor'),
+    }),
+    
+    extend: {
+      fontFamily: {
+        sans: [
+          'Nunito',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
+      inset: {
+        '100' : '100%',
+        ...defaultTheme.inset,
+      },
+      colors: {
+        bright_blue: '#2983FF',
+        dark_blue: {
+          default: '#718096',
+          active: '#2B325B',
+        },
+        border_color: '#adb2ba'
+      }
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    function({ addBase, config }) {
+      addBase({
+        'h1': { 
+          fontSize: config('theme.fontSize.2xl'),
+          fontWeight: config('theme.fontWeight.bold'),
+          margin: '0 0 ' + config('theme.spacing.8')
+        },
+        'h2': {
+          fontSize: config('theme.fontSize.lg'),
+          fontWeight: config('theme.fontWeight.bold'),
+          margin: '0 0 ' + config('theme.spacing.2')
+        },
+      })
+    },
+  ],
 }
